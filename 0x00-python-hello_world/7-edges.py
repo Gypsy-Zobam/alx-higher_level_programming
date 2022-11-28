9521cf5a94408a8c00c28cdf807db806ce3aed7a
Gypsy-Zobam <zo_bam@yahoo.com>
2022-11-28 09:06:15 -0800

    7-edges.py

--- 0x00-python-hello_world/7-edges.py
+++ 0x00-python-hello_world/7-edges.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 word = "Holberton"
-First_3_letter = [:3]
-Last_2_letter = [-2:]
-Middle_word = [1:-1]
+First_3_letter = word[:3]
+Last_2_letter = word[-2:]
+Middle_word = word[1:-1]
 print(f"First 3 letters: {word_first_3}")
 print(f"Last 2 letters: {word_last_2}")
 print(f"Middle word: {middle_word}")
