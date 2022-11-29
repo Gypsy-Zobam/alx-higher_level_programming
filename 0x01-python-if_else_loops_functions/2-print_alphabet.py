6ef6ee78723d08faeb293bcab57699e98536f236
Gypsy-Zobam <zo_bam@yahoo.com>
2022-11-29 12:40:06 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-for alpha_letters in range(97, 122):
-      if alpha_letters == ord(e) or alpha_letters == ord(q):
-         continue
-      print("{:c}".format(alpha_letters), end="")
+for i in range(97, 122):
+    print(chr(i), end=" ")
