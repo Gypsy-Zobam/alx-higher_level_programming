175a4d548f889c22faed1cde602355494e4a2fbe
Gypsy-Zobam <zo_bam@yahoo.com>
2022-12-01 18:35:43 -0800

    100-my_calculator.py

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    '''import sys
+    import sys
 
     nargs + len(sys.argv) - 1
     if nargs != 3:
@@ -10,11 +10,11 @@ if __name__ == "__main__":
     op = sys.argv[2]
     if op != '+' and op != '_' and op != '*' and op != '/':
         print("Unknown operator.Available operators: +, -, * and /")
-        sys.exit(1)'''
+        sys.exit(1)
+
     from calculator_1 import add, sub, mul, div
-    a, operator, b, = input('Enter calculation').split()
-    a = int(a)
-    b = int(b)
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
 
     if operator == "+":
         print("{} + {} = {}".format(a, b, add(a, b)))
@@ -22,8 +22,5 @@ if __name__ == "__main__":
         print("{} - {} = {}".format(a, b, sub(a, b)))
     elif operator == "*":
         print("{} * {} = {}".format(a, b, mul(a, b)))
-    elif operator == "/":
-        print("{} / {} = {}".format(a, b, div(a, b)))
-
     else:
-        print("Unknown operator.Available operators: +, -, * and /")
+        print("{} / {} = {}".format(a, b, div(a, b)))
