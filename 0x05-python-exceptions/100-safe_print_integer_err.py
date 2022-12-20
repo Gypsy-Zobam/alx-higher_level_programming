37204b18354af9883b20bf51bcf8d0b0bbe4d37b
Gypsy-Zobam <zo_bam@yahoo.com>
2022-12-19 17:51:12 -0800

    102-magic_calculation.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -8,5 +8,3 @@ def safe_print_integer_err(value):
     except (ValueError, TypeError) as error:
         print("Exception: {}".format(error), file=sys.stderr)
         return False
-
-    return True
