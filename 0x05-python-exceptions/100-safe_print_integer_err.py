371fb255c5c8a7a2da6d779b8baad7f8c891ed42
Gypsy-Zobam <zo_bam@yahoo.com>
2022-12-19 17:55:09 -0800

    102-magic_calculation.py

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -4,8 +4,9 @@ import sys
 
 def safe_print_integer_err(value):
     try:
-        print("{:d}".format(value))
-        return True
-    except (ValueError, TypeError):
-        print("Exception: {}".format(error), file=sys.stderr)
+        print('{:d}'.format(value))
+    except Exception as error:
+        print('Exception: {}'.format(error), file=sys.stderr)
         return False
+
+    return True
