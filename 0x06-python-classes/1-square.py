8a18b29913980b64466b71171ec9e1a7fa729421
Gypsy-Zobam <zo_bam@yahoo.com>
2022-12-21 17:48:24 -0800

    1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+'''Define a class Square'''
+
+
+class Square:
+    '''It is a square class with a private object attribute'''
+    def __init__(self, size):
+        self.size = size
+
+        return self.__size
