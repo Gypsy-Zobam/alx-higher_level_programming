16fc5eba4bbd40402612dc59fa852fe1f5faef3e
Gypsy-Zobam <zo_bam@yahoo.com>
2022-12-21 18:35:41 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python3
-'''Define a class Square'''
+"""Defines a class Square"""
 
 
 class Square:
-    ''' 
-    It is a square class with a private instance attribute.
-    Raise TypeError and ValueError exceptions
-    '''
+    """
+    it is a square class with a private object attribute.
+    It Checks the type of the argument and raises an exception on error
+    """
     def __init__(self, size=0):
+        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-
-        self.__size = size
