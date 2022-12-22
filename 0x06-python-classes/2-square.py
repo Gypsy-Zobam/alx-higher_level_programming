0b6ecfcf0623b76d08cb6f5453c2f36de90d4b0c
Gypsy-Zobam <zo_bam@yahoo.com>
2022-12-21 18:32:12 -0800

    2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,8 +8,9 @@ class Square:
     Raise TypeError and ValueError exceptions
     '''
     def __init__(self, size=0):
-        self.__size = size
         if type(size) is not int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
+
+        self.__size = size
