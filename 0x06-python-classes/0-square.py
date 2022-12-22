3f1385061bd4bbdf0614112dcf15dffb232a35d9
Gypsy-Zobam <zo_bam@yahoo.com>
2022-12-21 17:34:41 -0800

    0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-'''Define a class square'''
+"""Define a class square"""
 
 
 class square:
-    '''It is an empty class square'''
+    """It is an empty class square"""
     pass
