b3cd88e48b637a4210551ae77b2fc8d012d3cba7
Gypsy-Zobam <zo_bam@yahoo.com>
2023-01-04 18:42:50 -0800

    2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -53,6 +53,7 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
+
         self.__height = value
 
     """Instance methods that return the area
