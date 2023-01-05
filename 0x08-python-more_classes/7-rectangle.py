2e47e1ddd04ac1ba936abf2e6b07f794f747f393
Gypsy-Zobam <zo_bam@yahoo.com>
2023-01-04 19:48:12 -0800

    7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -5,7 +5,7 @@
 class Rectangle:
     """The Rectangle class"""
     number_of_instances = 0
-    print_symbol = '#'  
+    print_symbol = '#' 
 
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
