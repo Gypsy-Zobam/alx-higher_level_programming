aee194b477d4c48ade2b69bf44536b902f454ff7
Gypsy-Zobam <zo_bam@yahoo.com>
2023-01-04 20:14:25 -0800

    8-rectangle.py

--- 0x08-python-more_classes/8-rectangle.py
+++ 0x08-python-more_classes/8-rectangle.py
@@ -72,7 +72,7 @@ class Rectangle:
         print("Bye rectangle...")
 
     """Returns the biggest rectangle based on the area"""
-    @static method
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         if type(rect_1) != Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
