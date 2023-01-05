89f3296859c58c64e24d557c07f978ac51085e0b
Gypsy-Zobam <zo_bam@yahoo.com>
2023-01-04 19:38:01 -0800

    6-rectangle.py

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -4,7 +4,7 @@
 
 class Rectangle:
     """The Rectangle class"""
-    number_of _instances = 0
+    number_of_instances = 0
 
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
@@ -17,7 +17,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -30,7 +30,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) is not int:
+        if type(value) != int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
