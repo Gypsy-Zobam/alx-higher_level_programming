35aeadd8d47fe92e5c5f6c2664c97ed0606e815f
Gypsy-Zobam <zo_bam@yahoo.com>
2023-01-04 20:35:59 -0800

    9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -9,8 +9,8 @@ class Rectangle:
 
     def __init__(self, width=0, height=0):
         Rectangle.number_of_instances += 1
-        self.height = height
         self.width = width
+        self.height = height
 
     @property
     def width(self):
@@ -18,7 +18,7 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
             raise ValueError("width must be >= 0")
@@ -31,7 +31,7 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        if type(value) != int:
+        if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
@@ -48,7 +48,7 @@ class Rectangle:
             return 0
         return 2 * (self.width + self.height)
 
-    """Print the rectangle with the character #"""
+    """Returns a rectangle in string form"""
     def __str__(self):
         rect = ""
 
@@ -62,21 +62,21 @@ class Rectangle:
                 rect += '\n'
         return rect
 
-    """Return a string representation of the instance creation"""
+    """Returns a string representation of the instance creation"""
     def __repr__(self):
         return f"Rectangle({self.width}, {self.height})"
 
-    """Delete when an instance delete is run"""
+    """Runs when an instance is deleted"""
     def __del__(self):
         Rectangle.number_of_instances -= 1
         print("Bye rectangle...")
 
-    """Returns the biggest rectangle based on the area"""
+    """Returns the Rectangle instance with a bigger area"""
     @staticmethod
     def bigger_or_equal(rect_1, rect_2):
-        if type(rect_1) != Rectangle:
+        if type(rect_1) is not Rectangle:
             raise TypeError("rect_1 must be an instance of Rectangle")
-        elif type(rect_2) != Rectangle:
+        if type(rect_2) is not Rectangle:
             raise TypeError("rect_2 must be an instance of Rectangle")
 
         if rect_1.area() > rect_2.area():
@@ -89,4 +89,5 @@ class Rectangle:
     """Returns a new Rectangle instance with equal sides"""
     @classmethod
     def square(cls, size=0):
-        return Rectangle(size, size)
+        width = height = size
+        return cls(width, height)
