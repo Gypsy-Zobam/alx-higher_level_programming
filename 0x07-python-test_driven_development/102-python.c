e420f47f2f523d3d3b2a2172414e06f77e830325
Gypsy-Zobam <zo_bam@yahoo.com>
2023-01-06 13:03:02 -0800

    100-sorted_hash_table.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,28 +1,12 @@
-#include "Python.h"
+#!/usr/bin/python3
+"""Module lazy_matrix_mul
+Matrix multiplication using NumPy.
+"""
+import numpy
 
-/**
- * print_python_string - Prints information about Python strings.
- * @p: A PyObject string object.
- */
-void print_python_string(PyObject *p)
-{
-	long int length;
 
-	fflush(stdout);
-
-	printf("[.] string object info\n");
-	if (strcmp(p->ob_type->tp_name, "str") != 0)
-	{
-		printf("  [ERROR] Invalid String Object\n");
-		return;
-	}
-
-	length = ((PyASCIIObject *)(p))->length;
-
-	if (PyUnicode_IS_COMPACT_ASCII(p))
-		printf("  type: compact ascii\n");
-	else
-		printf("  type: compact unicode object\n");
-	printf("  length: %ld\n", length);
-	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
-}
+def lazy_matrix_mul(m_a, m_b):
+    """Multiplies m_a and m_b using
+    matmul, and returns the result.
+    """
+    return numpy.matmul(m_a, m_b)
