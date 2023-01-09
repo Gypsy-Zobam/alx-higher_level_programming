82f816f806304e7eeeddc8be883cc4221c7285cc
Gypsy-Zobam <zo_bam@yahoo.com>
2023-01-09 14:59:34 -0800

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
-'''Return the list of avaliable attributes and methods of an object
-'''
+"""Return the list of avaliable attributes and methods of an object
+"""
 
 
 def lookup(obj):
-    '''Returns a list object'''
+    """Returns a list object
     Args:
         obj(_type_): an object to be used
     Returns:
         dir(obj)
-    '''
+    """
     return dir(obj)
