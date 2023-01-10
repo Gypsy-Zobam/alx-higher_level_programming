dc4322bbf5b1226a35a2c2e9843960a11ac796bf
Gypsy-Zobam <zo_bam@yahoo.com>
2023-01-09 16:23:02 -0800

    6-base_geometry.py

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Improve Geometry"""
+
+
+class BaseGeometry:
+    """Contains a function area()"""
+
+    def area(self):
+        """Function not implemented"""
+
+        raise Exception("area() is not to beimplemented")
+
