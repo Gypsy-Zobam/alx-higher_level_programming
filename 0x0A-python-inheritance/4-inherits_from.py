cd041149202b395d476c46d12f4f3fc2b0837406
Gypsy-Zobam <zo_bam@yahoo.com>
2023-01-09 16:06:03 -0800

    4-inherits_from.py

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,14 @@
+#!/usrbinpython3
+""" Only sub class of"""
+
+
+def inherits_from(obj, a_class):
+    """
+    function that returns True if the object is an instance of a
+    class that inherited (directly or indirectly) from the specif
+    ied class ; otherwise False.
+    """
+    if type(obj) is a_class or not isinstance(obj, a_class):
+        return True
+    else:
+        return False
