80cac60d6fb4d7ff3dd988df9e60e668affc96f8
Gypsy-Zobam <zo_bam@yahoo.com>
2023-02-16 12:09:23 -0800

    10-top_score.sql

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -9,6 +9,6 @@
 CREATE TABLE IT NOT EXISTS second_table (id INT, name VARCHAR(256), score INT);
 INSERT INTO second_table (id, name, score)
 VALUE (id = 1, name = “John”, score = 10),
-	(id = 2, name = “Alex”, score = 3),
-	(id = 3, name = “Bob”, score = 14),
-	(id = 4, name = “George”, score = 8);
+(id = 2, name = “Alex”, score = 3),
+(id = 3, name = “Bob”, score = 14),
+(id = 4, name = “George”, score = 8);
