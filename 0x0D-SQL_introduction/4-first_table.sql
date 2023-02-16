3bb867ac5c20064fe75d070c4a389633bd4c3d23
Gypsy-Zobam <zo_bam@yahoo.com>
2023-02-16 12:04:13 -0800

    10-top_score.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- Creates a table called first_table in the current database in your MySQL server.
+-- first_table description:id INT, name VARCHAR(256).
+-- The database name will be passed as an argument of the mysql command.
+-- You are not allowed to use the SELECT or SHOW statements.
+
+CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256))
