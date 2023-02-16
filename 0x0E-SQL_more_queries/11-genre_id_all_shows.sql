4904ceb966076708c7b4cbebfbc484f79718d2db
Gypsy-Zobam <zo_bam@yahoo.com>
2023-02-15 20:55:11 -0800

    16-shows_by_genre.sql

--- 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
+++ 0x0E-SQL_more_queries/11-genre_id_all_shows.sql
@@ -5,7 +5,8 @@
 -- If a show doesn’t have a genre, display NULL.
 -- The database name will be passed as an argument of the mysql command
 
-SELECT tv_shows.title - tv_show_genres.genre_id
-FROM tv_show_genres RIGHT JOIN tv_shows
-ON tv_show_genres.show_id = tv_shows.id
+SELECT tv_shows.title, tv_show_genres.genre_id
+FROM tv_shows
+LEFT JOIN tv_show_genres
+ON tv_shows.id=tv_show_genres.show_id
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
