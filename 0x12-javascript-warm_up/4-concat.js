3d74aac1c918de42030d6e41ab5b3eda620fa6f0
Gypsy-Zobam <zo_bam@yahoo.com>
2023-03-13 16:14:15 -0700

    0-javascript_is_amazing.js

--- 0x12-javascript-warm_up/4-concat.js
+++ 0x12-javascript-warm_up/4-concat.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
-console.log(`${process.argv[2]} is ${process.argv[3]}`);
+const argv = require('process').argv;
+
+const arg1 = argv[2] ? argv[2] : 'undefined';
+const arg2 = argv[3] ? argv[3] : 'undefined';
+
+console.log(arg1.concat(' is ', arg2));
 
