0220b065884101974faa00534694ffd1f62a60a3
Gypsy-Zobam <zo_bam@yahoo.com>
2023-03-19 17:59:51 -0700

    14-model_city_fetch_by_state.py

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
+"""
+Defines a State model.
+Inherits from SQLAlchemy Base and links to the MySQL table states.
+"""
 
-'''
-    Python file that contains the class definition of a
-    State and an instance Base = declarative_base()
-'''
-
-import sqlalchemy
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -13,9 +11,11 @@ Base = declarative_base()
 
 
 class State(Base):
+    """Represents a state for a MySQL database.
+    __tablename__ (str): The name of the MySQL table to store States.
+    id (sqlalchemy.Integer): The state's id.
+    name (sqlalchemy.String): The state's name.
     """
-        Representation of a state.
-    """
-    __tablename__ = 'states'
+    __tablename__ = "states"
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
