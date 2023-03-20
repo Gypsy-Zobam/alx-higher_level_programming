9ae45a12ba3ebbf72e98a4170ac56d006ece0383
Gypsy-Zobam <zo_bam@yahoo.com>
2023-03-19 17:35:20 -0700

    102-relationship_cities_states_list.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,37 +1,24 @@
 #!/usr/bin/python3
-''' script for task 2
-    script should take 4 arguments: mysql username,
-    mysql password, database name and state name searched
-'''
-
-import MySQLdb
+"""
+Displays all values in the states table of the database hbtn_0e_0_usa
+whose name matches that supplied as argument.
+Usage: ./2-my_filter_states.py <mysql username> \
+                                <mysql password> \
+                                <database name> \
+                                <state name searched>
+"""
 import sys
+import MySQLdb
 
-
-def list_with_name():
-    ''' displays all values in the states table in hbtn db where name
-        matches the argument passed to the script
-    '''
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
-    state_name = sys.argv[4]
-    host = 'localhost'
-    port = 3306
-
-    db = MySQLdb.connect(host=host, user=username, passwd=password,
-                         db=db_name, port=port)
-    cur = db.cursor()
-    cur.execute(('SELECT * FROM states WHERE BINARY name = \'{}\'\
-                 ORDER BY id ASC;').format(state_name))
-    result = cur.fetchall()
-    cur.close()
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], port=3306, host="localhost",
+                         passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    c.execute("SELECT * FROM states WHERE name LIKE '{:s}' ORDER BY \
+    id ASC".format(sys.argv[4]))
+    states = c.fetchall()
+    for state in states:
+        if state[1] == sys.argv[4]:
+            print(state)
+    c.close()
     db.close()
-
-    if result:
-        for row in result:
-            print(row)
-
-
-if __name__ == '__main__':
-    list_with_name()
