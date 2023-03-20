aa9d5b9683135d412cfe6aa6333406d321d7b55e
Gypsy-Zobam <zo_bam@yahoo.com>
2023-03-19 20:42:18 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,19 +1,15 @@
 #!/usr/bin/python3
-"""
-takes in an argument and displays all values in the states table
-"""
-import MySQLdb
-import sys
+"""main file"""
+if __name__ == '__main__':
+    import sys
+    import MySQLdb
+    db = MySQLdb.connect(
+        user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+    cur.execute(
+        """SELECT * FROM states WHERE BINARY `name` = "{}" ORDER BY
+        states.id ASC""".format(sys.argv[4].strip("'")))
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    match = sys.argv[4]
-    myCursor = db.cursor()
-    myCursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
-                     ORDER BY states.id ASC".format(sys.argv[4]))
-    rows = myCursor.fetchall()
+    rows = cur.fetchall()
     for row in rows:
         print(row)
-    myCursor.close()
-    db.close()
